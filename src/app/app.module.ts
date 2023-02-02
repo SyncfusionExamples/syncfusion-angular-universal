3dc3264763160e9b437bd01c4d24920a6f04511b
Sathish Kumar Rajendran <sathishkumar.rajendran@syncfusion.com>
2023-02-02 20:09:10 +0530

    Updated the Syncfusion Angular Universal

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+import { GridModule, PagerModule } from '@syncfusion/ej2-angular-grids';
+
+
+@NgModule({
+  imports: [
+    BrowserModule.withServerTransition({ appId: 'tour-of-heroes' }),
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    GridModule, PagerModule
+  ],
+  declarations: [
+    AppComponent
+  ],
+  bootstrap: [ AppComponent ]
+})
+export class AppModule {
+
+}
